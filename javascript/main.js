2f7de2890734853358abdc249766dbbd59a92873
mbkosik <72941144+mbkosik@users.noreply.github.com>
2021-09-13 21:49:47 +0200

    js script + scss

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,139 @@
+const playBtn = document.querySelector(".stopwatch__buttons--play");
+const pauseBtn = document.querySelector(".stopwatch__buttons--pause");
+const stopBtn = document.querySelector(".stopwatch__buttons--stop");
+const clearBtn = document.querySelector(".stopwatch__buttons--clear");
+const archiveBtn = document.querySelector(".stopwatch__buttons--archive");
+
+const savedTime = document.querySelector(".stopwatch__time--saved")
+const timeList = document.querySelector(".stopwatch__time-list");
+
+const timeCounterSec = document.querySelector(".stopwatch__time--current span.s");
+const timeCounterMin = document.querySelector(".stopwatch__time--current span.m");
+
+const timeSavedSec = document.querySelector(".stopwatch__time--saved span.s");
+const timeSavedMin = document.querySelector(".stopwatch__time--saved span.m");
+
+let timeSec = 0; 
+let timeMin = 0; 
+
+let currentMin = 0; 
+let currentSec = 0; 
+
+let play = false;
+let pause = false;
+let stop = false;
+
+
+function startCount() {
+    if (play === false) {
+        counting(); 
+    } else {
+        return; 
+    }
+}
+
+function pauseCount() {
+    if (play === true) {
+        pause = true; 
+        if (currentMin <= 9) {
+            timeSavedMin.textContent = `0${currentMin}`
+        } else {
+            timeSavedMin.textContent = currentMin;
+        }
+
+        if (currentSec <= 9) {
+            timeSavedSec.textContent = `0${currentSec}`
+        } else {
+            timeSavedSec.textContent = currentSec;
+        }
+        savedTime.classList.add("active");
+        let newListElement = document.createElement("li");
+        newListElement.textContent = savedTime.textContent;
+        timeList.appendChild(newListElement); 
+    } else {
+        return; 
+    }
+}
+
+function stopCount() {
+    if (play === true) {
+        stop = true; 
+    } else {
+        stop = true;
+        startCount(); 
+    }
+}
+
+function clearCount() {
+    stopCount(); 
+    timeList.classList.remove("active"); 
+    timeList.textContent = "";
+    timeSavedSec.textContent = "";
+    timeSavedMin.textContent = ""; 
+    savedTime.classList.remove("active");
+}
+
+function showArchive() {
+    timeList.classList.add("active");
+}
+
+const counting = () => {
+
+    if (pause === true) {
+        pause = false; 
+        play = false; 
+        currentSec = 0; 
+        currentMin = 0; 
+        return; 
+    }
+
+    if (stop === true) {
+        stop = false; 
+        play = false; 
+        timeSec = 0; 
+        timeMin = 0; 
+        timeCounterMin.textContent = "00";
+        timeCounterSec.textContent = "00";
+        return; 
+    }
+
+    play = true; 
+
+    if (timeSec === 59) {
+        timeSec = -1;
+        timeMin++;
+        if (timeMin <= 9) {
+            timeCounterMin.textContent = `0${timeMin}`
+        } else {
+            timeCounterMin.textContent = timeMin;
+        }
+    }
+
+    if (currentSec == 59) {
+        currentSec = -1; 
+        currentMin++;
+    }
+ 
+    timeSec++;
+    currentSec++;
+    if (timeSec <= 9) {
+        timeCounterSec.textContent = `0${timeSec}`
+    } else {
+        timeCounterSec.textContent = timeSec;
+    }
+
+    setTimeout(counting, 500)
+}
+
+
+playBtn.addEventListener("click", startCount);
+pauseBtn.addEventListener("click", pauseCount);
+stopBtn.addEventListener("click", stopCount);
+clearBtn.addEventListener("click", clearCount);
+archiveBtn.addEventListener("click", showArchive);
+
+// WYŚWIETLANIE POP-UPA z INSTRUKCJĄ
+
 const infoBtn = document.querySelector(".fa-question");
 const modalCloseBtn = document.querySelector(".modal__close");
 const modal = document.querySelector(".shadow__modal");
@@ -11,3 +147,22 @@ const showModal = (e) => {
 }
 
 window.addEventListener("click", showModal);
+
+// ZMIANA MOTYWU KOLORU
+
+const changeColorBtn = document.querySelector(".fa-paint-brush");
+let root = document.documentElement;
+
+let colorsArray = ["#F05941", "#FFD369", "#27496D"];
+let i = 0;
+
+const changeColor = () => {
+    if (i === colorsArray.length - 1) {
+        i = -1; 
+    }
+    i++; 
+    root.style.setProperty('--main-color', colorsArray[i]);
+    console.log(i);
+}
+
+changeColorBtn.addEventListener("click", changeColor);
